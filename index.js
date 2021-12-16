7be2933d147973023c81fc685d1f4f18dd153fa1
Alaina <alaismit1@gmail.com>
2021-12-16 12:15:48 -0700

    firth commit

--- index.js
+++ index.js
@@ -10,14 +10,30 @@ const rollbar = new Rollbar({
 })
 
 
+let students = []
+
 
 const app = express()
+
+
+app.use(rollbar.errorHandler)
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))  //this is sending back the path to the html file
     rollbar.info('html file served successfully')
+})                                                          //path is a pre-built set of code that comes with node that functions as an object to let us do this. similar to requiring stuff like express.
 
-})                                                              //path is a pre-built set of code that comes with node that functions as an object to let us do this. similar to requiring stuff like express.
+app.post('/api/student', () => (req, res) => {
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('Student added succesfully', {author:'Alaina', type: 'manual'})    //create students array up top
+    res.status(200).send(students)
+})
+
 
 const port = process.env.PORT || 4545
 
